c50aadc20f1284f15015fe89c97286f51546cf96
Patrick <the.token.blackman@gmail.com>
2016-04-26 00:37:48 +0200

    Complete Filter Example

---
+++ CustomSortFilterModel.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2016-04-26T00:12:08
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = CustomSortFilterModel
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        window.cpp \
+    mysortfilterproxymodel.cpp
+
+HEADERS  += window.h \
+    mysortfilterproxymodel.h
---
+++ window.h
@@ -0,0 +1,48 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+
+class QAbstractItemModel;
+class QCheckBox;
+class QComboBox;
+class QDateEdit;
+class QGroupBox;
+class QLabel;
+class QLineEdit;
+class QTreeView;
+
+class MySortFilterProxyModel;
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Window(QWidget *parent = 0);
+    ~Window();
+
+    void setSourceModel(QAbstractItemModel * model);
+
+private slots:
+    void textFilterChanged();
+    void dateFilterChanged();
+
+private:
+    MySortFilterProxyModel *proxyModel;
+
+    QGroupBox *sourceGroupBox;
+    QGroupBox *proxyGroupBox;
+    QTreeView *sourceView;
+    QTreeView *proxyView;
+    QCheckBox *filterCaseSensitivityCheckBox;
+    QLabel *filterPatternLabel;
+    QLabel *fromLabel;
+    QLabel *toLabel;
+    QLineEdit *filterPatternLineEdit;
+    QComboBox *filterSyntaxComboBox;
+    QDateEdit *fromDateEdit;
+    QDateEdit *toDateEdit;
+};
+
+#endif // WINDOW_H
